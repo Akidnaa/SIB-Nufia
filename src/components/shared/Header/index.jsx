53f187269a8f0b58bbbb9e9bcd204b4780a1b31f
ANDHIKA THATA <m.dhikathata@gmail.com>
2025-10-23 18:47:43 +0700

    Tugas Pertemuan 3 REACTJS

--- src/components/shared/Header/index.jsx
+++ src/components/shared/Header/index.jsx
@@ -14,7 +14,7 @@ export default function Header() {
 
                 <ul className="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
                  <li><Link to="/" className="nav-link px-2">Home</Link></li>
-                 <li><Link to="/Books" className="nav-link px-2">Book</Link></li>
+                 <li><Link to="/Book" className="nav-link px-2">Book</Link></li>
                  <li><Link to="/Teams" className="nav-link px-2">Team</Link></li>
                  <li><Link to="/Contacts" className="nav-link px-2">Contact</Link></li>
                 </ul>
